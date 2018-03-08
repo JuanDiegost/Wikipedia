83c08fa5be455192416b01140170a17a36278095
JuanDiegost <molinajuan149@gmail.com>
2018-03-07 22:22:49 -0500

    Add materializae css

--- js/vendor.js
+++ js/vendor.js
@@ -1,6 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  Materialize.updateTextFields();
   var api = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=";
-  var end ="&prop=info&inprop=url&utf8=&format=json";
+  var end = "&prop=info&inprop=url&utf8=&format=json";
 
   $('#text-search').keypress(function (e) {
     if (e.which == 13) {
@@ -16,26 +17,25 @@ $(document).ready(function() {
     var keyword = $("#text-search").val();
     $("#text-search").val('');
     $.ajax({
-      url:  api+ keyword + end,
+      url: api + keyword + end,
       dataType: "jsonp",
-      success: function(response) {
+      success: function (response) {
         if (response.query.searchinfo.totalhits === 0) {
           showError(keyword);
         }
         else {
           showResults(response);
         }
-    }
-  });
-}
+      }
+    });
+  }
 
   function showResults(data) {
     $("#result").empty();
     for (var i = 0; i < data.query.search.length; i++) {
       var variable = data.query.search[i]
-    $("#result").append('<a href="https://en.wikipedia.org/?curid='+variable.pageid+'" target="_blank"><article id="'+variable.pageid+'"><h2>'+variable.title+"</h2><p>"+variable.snippet+"</p></article></a>");
+      $("#result").append('<div class="row"><div class="col s12 m7"><div class="card " id="' + variable.pageid + '"><div class="card-content hoverable"><span class="card-title">' + variable.title + '</span><p>' + variable.snippet + '</p></div><div class="card-action"><a href="https://en.wikipedia.org/?curid=' + variable.pageid + '" target="_blank">This is a link</a></div></div></div></div>');
+      /* <a href="https://en.wikipedia.org/?curid='+variable.pageid+'" target="_blank"><article id="'+variable.pageid+'"><h2>'+variable.title+"</h2><p>"+variable.snippet+"</p></article></a>");*/
     }
   }
-
-
 });
